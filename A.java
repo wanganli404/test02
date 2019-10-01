451f08952de61693458e8685d9891a31cc415771
wanganli404 <3464914951@qq.com>
2019-10-01 16:28:05 +0800

    第二次修改

--- A.java
+++ A.java
@@ -3,6 +3,6 @@ public class A{
 public static void main(string args[]){
         System.out.println("wanganli404");
         Sysstem.out.println("修改");
-	System.out.println("本地操作");
+	System.out.println("本地操作22");
 }
 }
